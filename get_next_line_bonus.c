b5761c2c1f0eb28e267fe98cf13436f08971ec50
Furkan Uyar <fuyar@k1m43s01.42istanbul.com.tr>
2023-10-30 19:23:05 +0300

    get_next_line

---
+++ get_next_line_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fuyar <fuyar@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 11:11:45 by fuyar             #+#    #+#             */
+/*   Updated: 2023/10/30 19:22:47 by fuyar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_readline(int fd, char *dst)
+{
+	int		i;
+	char	*buff;
+
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	i = 1;
+	while (!ft_strchr(dst, '\n') && i != 0)
+	{
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[i] = '\0';
+		dst = ft_strjoin(dst, buff);
+	}
+	free(buff);
+	return (dst);
+}
+
+char	*ft_getline(char *dst)
+{
+	int		i;
+	char	*ret;
+
+	i = 0;
+	if (!dst[i])
+		return (NULL);
+	while (dst[i] && dst[i] != '\n')
+		i++;
+	ret = malloc(sizeof(char) * (i + 2));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (dst[i] && dst[i] != '\n')
+	{
+		ret[i] = dst[i];
+		i++;
+	}
+	if (dst[i] == '\n')
+	{
+		ret[i] = dst[i];
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+char	*ft_getleft(char *dst)
+{
+	int		i;
+	int		c;
+	char	*ret;
+
+	i = 0;
+	while (dst[i] != '\n' && dst[i])
+		i++;
+	if (!dst[i])
+	{
+		free(dst);
+		return (NULL);
+	}
+	ret = malloc(sizeof(char) * (ft_strlen(dst) - i + 1));
+	if (!ret)
+		return (NULL);
+	i++;
+	c = 0;
+	while (dst[i])
+		ret[c++] = dst[i++];
+	ret[c] = '\0';
+	free(dst);
+	return (ret);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*str;
+	static char	*dst[9999];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	dst[fd] = ft_readline(fd, dst[fd]);
+	if (!dst[fd])
+		return (NULL);
+	str = ft_getline(dst[fd]);
+	dst[fd] = ft_getleft(dst[fd]);
+	return (str);
+}
